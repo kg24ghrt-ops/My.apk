9392bfd654bace349667c8a711a795db56a1d53a
kg24ghrt-ops <kg24ghrt@gmail.com>
2026-01-05 13:38:53 +0630

    Initial Hello World project with workflow

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.helloworld;
+
+import android.app.Activity;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-public class MainActivity extends AppCompatActivity {
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        TextView tv = new TextView(this);
+        tv.setText("Hello World!");
+        setContentView(tv);
     }
 }
