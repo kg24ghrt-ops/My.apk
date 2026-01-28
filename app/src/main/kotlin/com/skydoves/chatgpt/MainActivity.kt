039348ddb0f32f51b3e7990f34a23f1678cd267d
kg24ghrt-ops <kg24ghrt@gmail.com>
2026-01-28 19:57:29 +0630

    Initial commit: full project push

--- app/src/main/kotlin/com/skydoves/chatgpt/MainActivity.kt
+++ app/src/main/kotlin/com/skydoves/chatgpt/MainActivity.kt
@@ -1,19 +1,3 @@
-/*
- * Designed and developed by 2024 skydoves (Jaewoong Eum)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.skydoves.chatgpt
 
 import android.os.Bundle
@@ -22,16 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.runtime.CompositionLocalProvider
 import com.skydoves.chatgpt.core.designsystem.composition.LocalOnFinishDispatcher
 import com.skydoves.chatgpt.core.designsystem.theme.ChatGPTComposeTheme
-import com.skydoves.chatgpt.core.navigation.AppComposeNavigator
 import com.skydoves.chatgpt.ui.ChatGPTMain
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-  @Inject
-  internal lateinit var appComposeNavigator: AppComposeNavigator
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -40,7 +19,9 @@ class MainActivity : ComponentActivity() {
       CompositionLocalProvider(
         LocalOnFinishDispatcher provides { finish() }
       ) {
-        ChatGPTComposeTheme { ChatGPTMain(composeNavigator = appComposeNavigator) }
+        ChatGPTComposeTheme { 
+            ChatGPTMain(context = this) // pass the activity as context
+        }
       }
     }
   }
